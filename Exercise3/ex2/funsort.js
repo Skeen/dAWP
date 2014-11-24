126249e0ed3de663050a5dc30e6a09183a5963db
ViV3RRa <soren1@htomail.com>
2014-11-24 17:29:19 +0100

    added rapport.tex

--- Exercise3/ex2/funsort.js
+++ Exercise3/ex2/funsort.js
@@ -1,114 +1,60 @@
-function reset()
-{
-    var numberRow = document.getElementById('numberRow');
-    // Loop through all the spans
-    for(i = 0; i < numberRow.children.length; i++)
-    {
-        // Generate a random number between 1-10000
+function reset(){
+    for(i = 1; i <= 10; i++){
         var value = Math.floor((Math.random() * 10000) + 1);
-        // Set the span's innerHTMl to this number
-        document.getElementById("drag"+(i+1)).innerHTML = value;
+        var span = "#sp"+i;
+        $(span).text(value);
     }
 }
 
-function init()
-{
-    
-    for(i = 1; i <= 10; i++){
-        var drag = "#drag"+i;
-        $( drag ).draggable();
-    }
-
-    for(i = 1; i <= 10; i++){
-        var td = "#td"+i;
-        $( td ).droppable({
-            drop: function( event, ui ) {
-                alert("Dropped");
-                //$( this )
-                ui.position = {"top":100, "left":100};
-            }
+function init(){
+    $(function() {
+            $("#sortable").sortable({
+                update: function(event, ui){
+                    checkVictory();
+                }
+            });
+            $("#sortable").disableSelection();
         });
-    }
+
     reset();
 
-    if(typeof(Storage) !== "undefined")
-    {
+    if(typeof(Storage) !== "undefined"){
         var num_played = 0;
-        document.getElementById('test').innerHTML = "Games played: " + num_played;
+        document.getElementById('playedGames').innerHTML = "Games played: " + num_played;
         localStorage.setItem("played", num_played);
-    } 
-    else 
-    {
-        document.getElementById('test').innerHTML = "Sorry, your browser does not support Web Storage...";
+    } else {
+        document.getElementById('playedGames').innerHTML = "Sorry, your browser does not support Web Storage...";
     }
 }
 
-function allowDrop(ev)
-{
-    // Prevent the default browser action
-    ev.preventDefault();
-}
+function checkVictory(){
+    var isSorted = true;
 
-function drag(ev)
-{
-    ev.dataTransfer.setData("text", ev.target.id);
-}
+    $("#sortable span").each(function(){
+        $current = $(this);
+        $next = $current.parent().next().find("span");
 
-function drop(ev)
-{
-    // Prevent the default browser action
-    ev.preventDefault();
-    // The ID and HTML of the dragged
-    var draggedID = ev.dataTransfer.getData("text");
-    var draggedHTML = document.getElementById(draggedID).innerHTML;
-    // HTML of the 'onto' landing span.
-    var ontoHTML = ev.target.innerHTML;
-    // Swap their contents
-    ev.target.innerHTML = draggedHTML;
-    document.getElementById(draggedID).innerHTML = ontoHTML;
-    // Check if we're sorted
-    checkVictory();
-}
-
-function getValue(val)
-{
-    var tmp = val.match(/\d+/g);
-    return tmp[1];
-}
-
-function checkVictory()
-{
-    var numberRow = document.getElementById('numberRow');
-    // Start at 1, for us to be able to look back 1 when comparing
-    for(i = 1; i < numberRow.children.length; i++)
-    {
-        // Get i-1 as previous
-    	var prev = numberRow.children[i-1];
-	    var prevVal = parseInt(getValue(prev.innerHTML));
-        // Get i as current
-	    var cur = numberRow.children[i];
-	    var curVal = parseInt(getValue(cur.innerHTML));
-        // If the previous number is larger than the current, then we're not sorted.
-        if(prevVal > curVal) 
-        {
-            // And we return to avoid falling through the for loop
-            return;
+        if($next.length > 0){
+            if(parseInt($current.text(), 10) > parseInt($next.text(), 10)){
+                isSorted = false;
+            }
         }
-    }
-    // At this point, we know that the spans are indeed in sorted order
-    alert("VICTORIOUS!!!!!!");
-    // Reset the game, thereby readying for another game
-    reset();
-    // Up the number of games played
-    if(typeof(Storage) != "undefined")
-    {
-        var prevPlayed = localStorage.getItem("played");
-        var newPlayed = parseInt(prevPlayed)+1;
-        document.getElementById('test').innerHTML = "Games played: " + newPlayed;
-        localStorage.setItem("played", newPlayed);
-    } 
-    else 
-    {
-        document.getElementById('test').innerHTML = "Sorry, your browser does not support Web Storage...";
+       // console.log($current.text() + " > " + $next.text() + " is " + (parseInt($current.text(), 10) > parseInt($next.text(), 10)));
+    })
+    //console.log(isSorted);
+    //console.log("--------------------------------------------");
+
+    if(isSorted){
+        alert("VICTORIOUS!!!!!!");
+        reset();
+        // Up the number of games played
+        if(typeof(Storage) != "undefined"){
+            var prevPlayed = localStorage.getItem("played");
+            var newPlayed = parseInt(prevPlayed)+1;
+            document.getElementById('playedGames').innerHTML = "Games played: " + newPlayed;
+            localStorage.setItem("played", newPlayed);
+        } else {
+            document.getElementById('playedGames').innerHTML = "Sorry, your browser does not support Web Storage...";
+        }
     }
 }
