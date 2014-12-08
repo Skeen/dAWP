7d9cce81f17c677196a44dfa8d9ecb2f540ccd67
Rohde Fischer <rohdef@rohdef.dk>
2014-12-08 15:53:27 +0100

    Initial report with first exercise

---
+++ Exercise5/report/auto/report.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook "report"
+ (lambda ()
+    (TeX-run-style-hooks
+     "hyperref"
+     "minted"
+     "listings"
+     ""
+     "inputenc"
+     "utf8"
+     "latex2e"
+     "art10"
+     "article"
+     "10pt"
+     "a4paper")))
+
